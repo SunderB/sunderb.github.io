919436416a992c34e4a0fad1ccce7fc699d4a39c
SunderB <20426079+SunderB@users.noreply.github.com>
2016-11-16 17:26:02 +0000

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,70 @@
+<!--
+SunderB's Website
+Copyright SunderB 2016
+-->
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8 "name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Sunder's Website</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div id="main_wrapper">
+
+		<header id="top_header" >
+			<h1>SunderB's Website<h1>
+		</header>
+		
+		<nav id="top_menu">
+			<ul>
+				<li><a href="https://sunderb.github.io">Home</a></li>
+				<li><a href="https://sunderb.github.io/games">Games</a></li>
+				<li><a href="https://sunderb.github.io/repos">Repositories</a></li>
+			</ul>
+		</nav>
+		
+		<div id="new-div">
+		
+		<section id="main_section">
+			<article>
+				<header>
+					<hgroup>
+						<h1>Error 404</h1>
+						<h2>Page Not Found!</h2>
+					</hgroup>
+				</header>
+				<p>Looks like you are a bit lost, just return to <a href="https://sunderb.github.io">home</a> to find your way.</p>
+				<footer>
+					<p>-SunderB 14/11/16</p>
+				</footer>
+			</article>
+		<!--
+			<article>
+				<header>
+					<hgroup>
+						<h1>Title of Article 2</h1>
+						<h2>Subtitle for Article!</h2>
+					</hgroup>
+				</header>
+				<p>This is the second best article eva!</p>
+				<footer>
+					<p>Written by Sunder 8/9/16</p>
+					</footer>
+			</article>
+		-->
+		</section>
+	
+		<aside id="side_info">
+			<h2>News</h2>
+			<p>It is 2016!<p>
+		</aside>
+		
+		</div>
+		
+		<footer id="bottom_footer">
+			Copyright SunderB 2016
+		</footer>
+	</div id="main_wrapper">
+</body>
+</html>
