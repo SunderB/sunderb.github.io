416b228a0a7220a956287cbdc878f6f61323d914
SunderB <sunder@sunderb.me>
2023-01-16 23:23:44 +0000

    Remove unused header resizing code and jQuery

--- _includes/resize_main_section.js
+++
@@ -1,8 +0,0 @@
-// Adjust the main section width and article margins when there isn't a side bar
-$(document).ready(function(){
-  var sideBarExists = document.getElementById("side_info");
-  if (sideBarExists == null) {
-    $("#main_section").css({ "width": "100%"});
-    $("article footer").css({ "margin-right": "5px"});
-  }
-});
